c16548aac0699e843cb6898a19c0c566e73e4e91
kucing3112215 <alfarizi311215@gmail.com>
2025-06-30 12:16:15 +0700

    Update InsertProduct.java

--- Project_UAS/InsertProduct.java
+++ Project_UAS/InsertProduct.java
@@ -11,10 +11,10 @@ import javax.swing.JOptionPane;
 
 
 /**
- *  @author Alfarizi Bima Setyawan
+ *
+ * @author Alfarizi Bima Setyawan
  * Kelas Pemrogaman Berorientasi Objek - 2B
  * Alfarizi311215@gmail.com
- * 
  */
 public class InsertProduct extends javax.swing.JFrame {
 
